d2a7c8501900ae3234de72241974868197599668
Rasmus <rasmus.laitila@student.hamk.fi>
2024-02-05 12:38:44 +0200

    login etenee

--- admin/henkilölista.php
+++ admin/henkilölista.php
@@ -1,5 +1,5 @@
 <?php
-$initials=parse_ini_file("../php/muuta/ht.tieto.ini");
+$initials=parse_ini_file("./ht.tieto.ini");
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 try{
     $yhteys=mysqli_connect($initials["databaseserver"], $initials["username"], $initials["password"], $initials["database"]);}
@@ -7,11 +7,13 @@ catch(Exception $e){
     header("Location:../php/muuta/yhteysvirhe.html");
     exit;
 }
+include "muuta/header.html";
 $numero=1;
-$tulos=mysqli_query($yhteys, "select * from users");
+$tulos=mysqli_query($yhteys, "select * from user");
 while ($rivi=mysqli_fetch_object($tulos)){
     print "$numero. $rivi->etunimi $rivi->sukunimi <a href='./mods/remuser.php?poistettava=$rivi->id'>Poista</a> <a href='./mods/moduser.php?muokattava=$rivi->id'>Muokkaa</a></p>";
     $numero=$numero+1;
 }
 mysqli_close($yhteys);
+include "muuta/footer.html";
 ?>
