d46a1d2075473a7fee8959be2d3a0abf83a4ffba
iiro23001 <iiro23001@student.hamk.fi>
2024-02-07 11:20:44 +0200

    admin kuvia ja virhe.html

--- admin/users.php
+++ admin/users.php
@@ -1,30 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="descripton" content="Experience exceptional grooming at our barber shop. Our skilled barbers offer modern and classic haircuts and grooming services. Step into a welcoming atmosphere where style meets precision. Book your appointment for a fresh look today!">
-    <title>(ADMIN) HAMK Hiusparatiisi</title>
-    <link rel="stylesheet" href="admin.css">
-
-</head>
-<body>
-    <!--Hiuskaiedas HAMK tervetuloa-->
-
-    <header>
-        <h1> (ADMIN) Hamkin Hiusparatiisi</h1>
-    </header>
-      <nav class="index">
-        <a href="../index.html">etusivulle</a> 
-        <a href="./lisaa.php">lisää käyttäjä</a> 
-
-    </nav> <!--First navigation box for the frontpage-->
-
-    <?php
+<?php
+include "users.html";
 $initials = parse_ini_file("./ht.as.ini");
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 try {
-    $yhteys = mysqli_connect($initials["databaseserver"], $initials["username"], $initials["password"], $initials["database"]);
+    $yhteys = mysqli_connect($initials["databaseserver"], $initials["name"], $initials["password"], $initials["database"]);
 } catch(Exception $e) {
     header("Location:./virhe.html");
     exit;
@@ -40,16 +19,7 @@ while ($rivi = mysqli_fetch_object($tulos)) {
 }
 
 echo "</table>";
+include "adminfooter.html";
 
 mysqli_close($yhteys);
 ?>
-
-    
-    <footer>
-        <a href="https://github.com/RasmusLaitilaHAMK/WebDevTeam6">© 2024 Rasmus Laitila, Iiro Käki, Lauri Raatikainen</a>
-        <!--Link to github-->
-    </footer>
-</body>
-<!--    echo "ID= " . $rivi->ID . " Name= " . $rivi->name . " Password= " . $rivi->pwd . " Phonenumber= " . $rivi->mobile ." " . "<a href='./poistauser.php?poistettava=" . $rivi->ID . "'>Poista</a> <a href='./muokkaauser.php?muokattava=" . $rivi->ID . "'>Muokkaa</a></p>";
--->
-</html>
